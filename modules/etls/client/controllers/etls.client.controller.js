2274ad82c52ac97934f648b1dce49a645da9d41c
AmbroiseKritz <ambroise.petitjean@gmail.com>
2017-02-07 14:31:40 +0100

    [soos] init indexeddb

--- modules/etls/client/controllers/etls.client.controller.js
+++ modules/etls/client/controllers/etls.client.controller.js
@@ -21,13 +21,13 @@
       manualColumnResize: true,
       contextMenu: false,
       afterChange: 'afterChange',
-      dropdownMenu: true
+      dropdownMenu: true,
       // performance tip: set constant size
-      /*      colWidths: 80,
-            rowHeights: 23,
-            // performance tip: turn off calculations
-            autoRowSize: false,
-            autoColSize: false,*/
+      /*       colWidths: 80,
+                  rowHeights: 23,
+                  // performance tip: turn off calculations
+      /*            autoRowSize: false,
+                  autoColSize: false*/
     };
 
 
@@ -40,9 +40,57 @@
       return html;
     }
 
-    
-    vm.upload = function (file) {
 
+
+    listtable()
+
+    function listtable() {
+      alasql('ATTACH INDEXEDDB DATABASE data; \
+        USE' + 'data' + ';', function () {
+        // List Table from IndexedDB
+        alasql.promise('SHOW TABLES FROM data')
+          .then(function (res) {
+            vm.listTable = res;
+          }).catch(function (err) {
+            console.log('Error:', err);
+          });
+      });
+    }
+
+
+
+    /*    function listtable() {
+          alasql('ATTACH INDEXEDDB DATABASE data; \
+            USE data;', function () {
+            // List Table from IndexedDB
+            alasql.promise('SHOW TABLES FROM data')
+              .then(function (res) {
+                vm.listTable = res;
+              }).catch(function (err) {
+                console.log('Error:', err);
+              });
+          });
+        }
+    */
+
+    loadtable();
+
+    function loadtable() {
+      alasql('ATTACH INDEXEDDB DATABASE data; \
+        USE data; \
+        ', function () {
+        // Select data from IndexedDB
+        alasql.promise('SELECT * FROM etl LIMIT 100')
+          .then(function (res) {
+            vm.metadata = Object.keys(res[0]);
+            vm.data = res;
+          }).catch(function (err) {
+            console.log('Error:', err);
+          });
+      });
+    }
+
+    vm.upload = function (file) {
       alasql('CREATE INDEXEDDB DATABASE IF NOT EXISTS data;\
         ATTACH INDEXEDDB DATABASE data; \
         USE data; \
@@ -56,12 +104,28 @@
             vm.data = res;
             console.log('data', vm.data);
             console.log('metadata', vm.metadata);
+          }).catch(function (err) {
+            console.log('Error:', err);
           });
       });
-
-
     };
 
+
+    /*    testcalc()
+
+        function testcalc() {
+          alasql('ATTACH INDEXEDDB DATABASE data; \
+            USE data; \
+            ', function () {
+            // Select data from IndexedDB
+            alasql.promise('SELECT Order_Priority , SUM([Shipping Cost]) AS payout FROM etl GROUP BY Order_Priority ')
+              .then(function (res) {
+                console.log('result', res)
+              }).catch(function (err) {
+                console.log('Error:', err);
+              });
+          });
+        }*/
 
 
   }
