00492b68b8fbac82c8d43a21b8ab73c074de7845
jeremy.burier@gmail.com <jeremy.burier@gmail.com>
2017-02-24 12:17:09 +0100

    Add thumbnail in png (+ svg) and the base64 encoding (if needed)

--- modules/etls/client/directives/build-chart.client.directive.js
+++ modules/etls/client/directives/build-chart.client.directive.js
@@ -15,26 +15,41 @@
       template: '<highchart id="{{chartId}}" config="chartConfig"></highchart>',
 
       link: function(scope, element, attrs) {
-        var thumbnail = saveAsBinary(element.children().children()[0].innerHTML);
-        scope.chartConfig.thumbnail = thumbnail;
-
-        /*scope.$watch('chartConfig', function(newValue, oldValue) {
+        scope.$watch('chartConfig', function(newValue, oldValue) {
           if (newValue) {
-            var thumbnail = saveAsBinary(element.children().children()[0].innerHTML);
-            scope.chartConfig.thumbnail = thumbnail;
+            var svgHTML = element.children().children()[0].innerHTML;
+            // TODO SVG version ?
+            var svgThumbnail = saveAsBinary(svgHTML);
+            scope.chartConfig.thumbnail = svgThumbnail;
+
+            // TODO PNG version ?
+            var pngThumbnail = svgHTML;
+            var svg = element.children().children().children()[0];
+            var width = svg.getAttribute('width');
+            var height = svg.getAttribute('height');
+            var imgSrc = 'data:image/svg+xml;base64,' + btoa(pngThumbnail);
+            var canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            var context = canvas.getContext('2d');
+            var image = new Image;
+            image.src = imgSrc;
+
+            image.onload = function() {
+              context.drawImage(image, 0, 0);
+              var canvasdata = canvas.toDataURL('image/png');
+              // scope.chartConfig.thumbnail = canvasdata;
+            };
           }
+        }, true);
 
-          //$('#binaryImage').attr('src', thumbnail);
-        }, true);*/
+        function saveAsBinary(elem) {
+          var svg = 'data:image/svg+xml,' + elem;
+          // TODO To Base64 encode for future record ?
+          // var svg = 'data:image/svg+xml;base64,' + btoa(elem);
 
-        function saveAsBinary(elem){
-          //var svg = document.getElementById(chartId).children[0].innerHTML;
-          //return svg = "data:image/svg+xml,"+svg;
-
-          var svg;
-
-          return svg = "data:image/svg+xml,"+elem;
-        };
+          return svg;
+        }
       }
 
     };
